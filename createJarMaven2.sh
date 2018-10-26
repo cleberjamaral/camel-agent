de40c8e2aaba5c685dcb9453b27ff3e2039425d6
Cleber <cleber@Cleber-Amarals-iMac.local>
2018-10-26 15:55:26 -0300

    Updating maven2 repo after pom fixed to group nz.ac.otago.infosci

--- createJarMaven2.sh
+++ createJarMaven2.sh
@@ -1,6 +1,21 @@
 !#/bin/bash
 
+read -p "
 
-read -p "Attention: it is suposing you are in the branch 'maven2' of camel-agent repository and on the root there is the 'master' branch with all project files and you have built the jar using 'mvn install' on camel-agent-component sub-project"
+ATTENTION: 
+It is assuming you are in the branch 'maven2' of camel-agent repository.
+On the root there is the 'master' branch with all project files.
+You have built the jar using 'mvn install' on camel-agent-component sub-project
+
+
+PRESS ANY KEY TO CONTINUE
+"
 
 mvn install:install-file -DgroupId=nz.ac.otago.infosci -DartifactId=camelagent -Dversion=1.1.3 -Dfile=../camel-agent/camel-agent-component/build/libs/camel-agent-component.jar -Dpackaging=jar -DlocalRepositoryPath=. -DcreateChecksum=true -DpomFile=../camel-agent/camel-agent-component/pom.xml
+
+echo "
+
+Process done!
+
+"
+
